6ef2a09cbf27340e132eccabee912c0c5020e320
Jeremy Chua <jeremy-chua@hotmail.com>
2019-05-02 06:28:23 +0800

    first commit

---
+++ riches/scan.bat
@@ -0,0 +1,15 @@
+@echo off
+echo We assume that sourceanalyzer is in your local path
+echo If it is not, update your path to include SCA.
+echo
+echo Cleaning up...
+sourceanalyzer -b riches -verbose -clean
+REM ant clean
+
+echo Translating and Scanning...
+REM ant all
+REM sourceanalyzer -findbugs -cp "jsplibs/**/*.jar;lib/**/*.jar;WEB-INF/lib/**/*.jar" -source 1.5 -java-build-dir "WEB-INF/classes" WEB-INF/ js/ jsplibs/ login/ pages/ config/ -f scanResults.fpr
+
+sourceanalyzer -b riches -verbose -source 1.8 -cp "jsplibs/**/*.jar;lib/**/*.jar;WEB-INF/lib/**/*.jar" WEB-INF/ js/ jsplibs/ login/ pages/ config/
+
+sourceanalyzer -b riches -verbose -mt -scan -f riches_results.fpr
