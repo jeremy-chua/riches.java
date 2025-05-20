f217ffd02643099c28c370baf9b46fec96fc94d7
Jeremy Chua <jeremy-chua@hotmail.com>
2025-05-21 00:07:31 +0800

    fix sqli location service

--- riches/WEB-INF/src/java/com/fortify/samples/riches/model/LocationService.java
+++ riches/WEB-INF/src/java/com/fortify/samples/riches/model/LocationService.java
@@ -19,130 +19,117 @@ import net.sf.hibernate.expression.Expression;
 
 import java.util.logging.Logger;
 
-public class LocationService
-{
+public class LocationService {
     final private static Logger log = Logger.getLogger(ConnectionFactory.class.getName());
 
-    private static void safeClose(Session session){
-        try{
-            if(session!=null)
+    private static void safeClose(Session session) {
+        try {
+            if (session != null)
                 session.close();
-        }
-        catch(HibernateException e)
-        {
-            //Nothing we can do... we might log this
+        } catch (HibernateException e) {
+            // Nothing we can do... we might log this
             log.warning("Unable to close Session in LocationService!");
         }
     }
 
-    private static void safeCloseConnection(Connection conn){
-        try{
-            if(conn!=null)
+    private static void safeCloseConnection(Connection conn) {
+        try {
+            if (conn != null)
                 conn.close();
-        }
-        catch(SQLException e)
-        {
-            //Nothing we can do... we might log this
+        } catch (SQLException e) {
+            // Nothing we can do... we might log this
             log.warning("Unable to close Connection in AdminUtils!");
         }
     }
 
-    private static void safeCloseStatement(Statement statement){
-        try{
-            if(statement!=null)
+    private static void safeCloseStatement(Statement statement) {
+        try {
+            if (statement != null)
                 statement.close();
-        }
-        catch(Exception e)
-        {
-            //Nothing we can do... we might log this
+        } catch (Exception e) {
+            // Nothing we can do... we might log this
             log.warning("Unable to close Statement in AdminUtils!");
         }
     }
 
-    public static List findAtmByZip(String zip)
-    {
-        List locations=null;
-        Session session=null;
-        try{
+    public static List findAtmByZip(String zip) {
+        List locations = null;
+        Session session = null;
+        try {
             session = ConnectionFactory.getInstance().getSession();
             String queryStr = "from Location where atm = 'Yes' and zip ='" + zip + "'";
             Query query = session.createQuery(queryStr);
             locations = query.list();
-        }
-        catch (Exception e){}
-        finally{
-            if(session!=null)
+        } catch (Exception e) {
+        } finally {
+            if (session != null)
                 safeClose(session);
         }
         return locations;
     }
 
-    public static List findBranchByZip(String zip) throws Exception
-    {
-        List branches=null;
-        Session session=null;
-        try
-        {
+    public static List findBranchByZip(String zip) throws Exception {
+        List branches = null;
+        Session session = null;
+        try {
             session = ConnectionFactory.getInstance().getSession();
             Criteria criteria = session.createCriteria(Location.class);
-            criteria.add(Expression.eq("branch",  "Yes"));
+            criteria.add(Expression.eq("branch", "Yes"));
             criteria.add(Expression.eq("zip", zip));
-            branches=criteria.list();
-        }
-        finally{
-            if(session!=null)
+            branches = criteria.list();
+        } finally {
+            if (session != null)
                 safeClose(session);
         }
         return branches;
     }
 
-    /* Example on how prepareStatement can be misused in such a way that the query is still vuln to SQLi */
-    public static List findByZip(String zip) throws Exception
-    {
-        Connection conn=null;
+    /*
+     * Example on how prepareStatement can be misused in such a way that the query
+     * is still vuln to SQLi
+     */
+    public static List findByZip(String zip) throws Exception {
+        Connection conn = null;
         PreparedStatement statement = null;
         ArrayList locations = new ArrayList();
 
-        try{
+        try {
             conn = ConnFactory.getInstance().getConnection();
 
-            String queryStr = "SELECT * FROM location WHERE zip = '" + zip  + "'";
+            String queryStr = "SELECT * FROM location WHERE zip = ?";
             statement = conn.prepareStatement(queryStr);
+            statement.setString(1, zip); // 20250519 - JC fix
             ResultSet rs = statement.executeQuery();
-            while (rs.next())
-            {
-                locations.add(new Location(rs.getString("address"), rs.getString("city"), rs.getString("state"), rs.getString("zip"), rs.getString("atm"), rs.getString("branch")));
+            while (rs.next()) {
+                locations.add(new Location(rs.getString("address"), rs.getString("city"), rs.getString("state"),
+                        rs.getString("zip"), rs.getString("atm"), rs.getString("branch")));
             }
-        }
-        finally{
+        } finally {
             safeCloseStatement(statement);
             safeCloseConnection(conn);
         }
 
         return locations;
 
-
     }
 
-
-    public static List findAtmByAddress(String address, String city, String state) throws Exception
-    {
-        Connection conn=null;
+    public static List findAtmByAddress(String address, String city, String state) throws Exception {
+        Connection conn = null;
         Statement statement = null;
         ArrayList locations = new ArrayList();
 
-        try{
+        try {
             conn = ConnFactory.getInstance().getConnection();
 
-            String queryStr = "SELECT * FROM location WHERE branch = 'Yes' AND state = '" + state + "' AND city = '" + city + "' AND address = '" + address + "'";
+            String queryStr = "SELECT * FROM location WHERE branch = 'Yes' AND state = '" + state + "' AND city = '"
+                    + city + "' AND address = '" + address + "'";
             statement = conn.createStatement();
             ResultSet rs = statement.executeQuery(queryStr);
-            while (rs.next())
-            {
-                locations.add(new Location(rs.getString("address"), rs.getString("city"), rs.getString("state"), rs.getString("zip"), rs.getString("atm"), rs.getString("branch")));
+            while (rs.next()) {
+                locations.add(new Location(rs.getString("address"), rs.getString("city"), rs.getString("state"),
+                        rs.getString("zip"), rs.getString("atm"), rs.getString("branch")));
             }
-        } 
-        finally{
+        } finally {
             safeCloseStatement(statement);
             safeCloseConnection(conn);
         }
@@ -150,21 +137,19 @@ public class LocationService
         return locations;
     }
 
-    public static List findBranchByAddress(String address, String city, String state) throws Exception
-    {
-        List branches=null;
-        Session session=null;
-        try{
+    public static List findBranchByAddress(String address, String city, String state) throws Exception {
+        List branches = null;
+        Session session = null;
+        try {
             session = ConnectionFactory.getInstance().getSession();
             Criteria criteria = session.createCriteria(Location.class);
-            criteria.add(Expression.eq("branch",  "Yes"));
+            criteria.add(Expression.eq("branch", "Yes"));
             criteria.add(Expression.eq("address", address));
             criteria.add(Expression.eq("city", city));
             criteria.add(Expression.eq("state", state));
-            branches=criteria.list();
-        }
-        finally{
-            if(session!=null)
+            branches = criteria.list();
+        } finally {
+            if (session != null)
                 safeClose(session);
         }
         return branches;
