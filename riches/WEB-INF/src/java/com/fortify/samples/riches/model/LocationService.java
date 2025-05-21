1557f1621caaae902a71fc2ea14cccc8fa027616
Jeremy Chua <jeremy-chua@hotmail.com>
2025-05-22 02:52:12 +0800

    updated gitignore

--- riches/WEB-INF/src/java/com/fortify/samples/riches/model/LocationService.java
+++ riches/WEB-INF/src/java/com/fortify/samples/riches/model/LocationService.java
@@ -3,6 +3,8 @@ package com.fortify.samples.riches.model;
 import com.fortify.samples.riches.database.ConnFactory;
 import com.fortify.samples.riches.database.ConnectionFactory;
 
+import freemarker.core.CommandLine;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.sql.Connection;
@@ -19,130 +21,135 @@ import net.sf.hibernate.expression.Expression;
 
 import java.util.logging.Logger;
 
-public class LocationService
-{
+public class LocationService {
     final private static Logger log = Logger.getLogger(ConnectionFactory.class.getName());
 
-    private static void safeClose(Session session){
-        try{
-            if(session!=null)
+    // Password in comment
+    // The database password is 123456
+
+    // JC - hardcoded secret and password test
+    private static final String SECRET = "1234567890abcdefg";
+    private static final String KEY = "secret_key";
+    private static final String SECRET_KEY = "secret_key";
+    private static final String PASSWORD = "P@$$w0rd!";
+    private static final String PSWD = "1234";
+    private static final String PWD = "1234";
+
+    private static void safeClose(Session session) {
+        try {
+            if (session != null)
                 session.close();
-        }
-        catch(HibernateException e)
-        {
-            //Nothing we can do... we might log this
+        } catch (HibernateException e) {
+            // Nothing we can do... we might log this
             log.warning("Unable to close Session in LocationService!");
         }
     }
 
-    private static void safeCloseConnection(Connection conn){
-        try{
-            if(conn!=null)
+    private static void safeCloseConnection(Connection conn) {
+        try {
+            if (conn != null)
                 conn.close();
-        }
-        catch(SQLException e)
-        {
-            //Nothing we can do... we might log this
+        } catch (SQLException e) {
+            // Nothing we can do... we might log this
             log.warning("Unable to close Connection in AdminUtils!");
         }
     }
 
-    private static void safeCloseStatement(Statement statement){
-        try{
-            if(statement!=null)
+    private static void safeCloseStatement(Statement statement) {
+        try {
+            if (statement != null)
                 statement.close();
-        }
-        catch(Exception e)
-        {
-            //Nothing we can do... we might log this
+        } catch (Exception e) {
+            // Nothing we can do... we might log this
             log.warning("Unable to close Statement in AdminUtils!");
         }
     }
 
-    public static List findAtmByZip(String zip)
-    {
-        List locations=null;
-        Session session=null;
-        try{
+    public static List findAtmByZip(String zip) {
+        List locations = null;
+        Session session = null;
+        try {
             session = ConnectionFactory.getInstance().getSession();
             String queryStr = "from Location where atm = 'Yes' and zip ='" + zip + "'";
             Query query = session.createQuery(queryStr);
             locations = query.list();
-        }
-        catch (Exception e){}
-        finally{
-            if(session!=null)
+        } catch (Exception e) {
+        } finally {
+            if (session != null)
                 safeClose(session);
         }
         return locations;
     }
 
-    public static List findBranchByZip(String zip) throws Exception
-    {
-        List branches=null;
-        Session session=null;
-        try
-        {
+    public static List findBranchByZip(String zip) throws Exception {
+        List branches = null;
+        Session session = null;
+        try {
             session = ConnectionFactory.getInstance().getSession();
             Criteria criteria = session.createCriteria(Location.class);
-            criteria.add(Expression.eq("branch",  "Yes"));
+            criteria.add(Expression.eq("branch", "Yes"));
             criteria.add(Expression.eq("zip", zip));
-            branches=criteria.list();
-        }
-        finally{
-            if(session!=null)
+            branches = criteria.list();
+        } finally {
+            if (session != null)
                 safeClose(session);
         }
         return branches;
     }
 
-    /* Example on how prepareStatement can be misused in such a way that the query is still vuln to SQLi */
-    public static List findByZip(String zip) throws Exception
-    {
-        Connection conn=null;
+    /*
+     * Example on how prepareStatement can be misused in such a way that the query
+     * is still vuln to SQLi
+     */
+    public static List findByZip(String zip) throws Exception {
+        Connection conn = null;
         PreparedStatement statement = null;
         ArrayList locations = new ArrayList();
 
-        try{
+        try {
             conn = ConnFactory.getInstance().getConnection();
 
-            String queryStr = "SELECT * FROM location WHERE zip = '" + zip  + "'";
+            String queryStr = "SELECT * FROM location WHERE zip = '" + zip + "'";
             statement = conn.prepareStatement(queryStr);
             ResultSet rs = statement.executeQuery();
-            while (rs.next())
-            {
-                locations.add(new Location(rs.getString("address"), rs.getString("city"), rs.getString("state"), rs.getString("zip"), rs.getString("atm"), rs.getString("branch")));
+            while (rs.next()) {
+                locations.add(new Location(rs.getString("address"), rs.getString("city"), rs.getString("state"),
+                        rs.getString("zip"), rs.getString("atm"), rs.getString("branch")));
             }
-        }
-        finally{
+        } finally {
             safeCloseStatement(statement);
             safeCloseConnection(conn);
         }
 
-        return locations;
+        // Test cmd injection
+        try {
+            Runtime.getRuntime().exec("ls -lah " + zip);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return locations;
 
     }
 
-
-    public static List findAtmByAddress(String address, String city, String state) throws Exception
-    {
-        Connection conn=null;
+    public static List findAtmByAddress(String address, String city, String state) throws Exception {
+        Connection conn = null;
         Statement statement = null;
         ArrayList locations = new ArrayList();
 
-        try{
+        try {
             conn = ConnFactory.getInstance().getConnection();
 
-            String queryStr = "SELECT * FROM location WHERE branch = 'Yes' AND state = '" + state + "' AND city = '" + city + "' AND address = '" + address + "'";
+            String queryStr = "SELECT * FROM location WHERE branch = 'Yes' AND state = '" + state + "' AND city = '"
+                    + city + "' AND address = '" + address + "'";
             statement = conn.createStatement();
             ResultSet rs = statement.executeQuery(queryStr);
-            while (rs.next())
-            {
-                locations.add(new Location(rs.getString("address"), rs.getString("city"), rs.getString("state"), rs.getString("zip"), rs.getString("atm"), rs.getString("branch")));
+            while (rs.next()) {
+                locations.add(new Location(rs.getString("address"), rs.getString("city"), rs.getString("state"),
+                        rs.getString("zip"), rs.getString("atm"), rs.getString("branch")));
             }
-        } 
-        finally{
+        } finally {
             safeCloseStatement(statement);
             safeCloseConnection(conn);
         }
@@ -150,21 +157,19 @@ public class LocationService
         return locations;
     }
 
-    public static List findBranchByAddress(String address, String city, String state) throws Exception
-    {
-        List branches=null;
-        Session session=null;
-        try{
+    public static List findBranchByAddress(String address, String city, String state) throws Exception {
+        List branches = null;
+        Session session = null;
+        try {
             session = ConnectionFactory.getInstance().getSession();
             Criteria criteria = session.createCriteria(Location.class);
-            criteria.add(Expression.eq("branch",  "Yes"));
+            criteria.add(Expression.eq("branch", "Yes"));
             criteria.add(Expression.eq("address", address));
             criteria.add(Expression.eq("city", city));
             criteria.add(Expression.eq("state", state));
-            branches=criteria.list();
-        }
-        finally{
-            if(session!=null)
+            branches = criteria.list();
+        } finally {
+            if (session != null)
                 safeClose(session);
         }
         return branches;
--- riches/scan.sh
+++ riches/scan.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
-echo We assume that sourceanalyzer is in your local path
-echo If it is not, update your path to include SCA.
-echo ANT should also be installed on your local machine.
+# #!/bin/bash
+# echo We assume that sourceanalyzer is in your local path
+# echo If it is not, update your path to include SCA.
+# echo ANT should also be installed on your local machine.
 
 
-echo Cleaning up...
-ant clean
-sourceanalyzer -b riches -clean
+# echo Cleaning up...
+# ant clean
+# sourceanalyzer -b riches -clean
 
-echo Translating and Scanning...
-ant all
-sourceanalyzer -findbugs -cp "jsplibs/**/*.jar:lib/**/*.jar:WEB-INF/lib/**/*.jar" -source 1.5 -java-build-dir "WEB-INF/classes" WEB-INF/ js/ jsplibs/ login/ pages/ config/ -f scanResults.fpr
+# echo Translating and Scanning...
+# ant all
+# sourceanalyzer -findbugs -cp "jsplibs/**/*.jar:lib/**/*.jar:WEB-INF/lib/**/*.jar" -source 1.5 -java-build-dir "WEB-INF/classes" WEB-INF/ js/ jsplibs/ login/ pages/ config/ -f scanResults.fpr
+
+sourceanalyzer -b riches -verbose -source 1.8 -cp "jsplibs/**/*.jar;lib/**/*.jar;WEB-INF/lib/**/*.jar" WEB-INF/ js/ jsplibs/ login/ pages/ config/
+
+sourceanalyzer -b riches -verbose  -scan -f riches_results.fpr
